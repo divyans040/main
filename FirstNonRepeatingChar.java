fb8faf9df486f2c559af5ac3b0e5fec127b632b3
divyans040 <divyansh9005854908@gmail.com>
2024-06-07 17:45:27 +0530

    Update FirstNonRepeatingChar.java

--- FirstNonRepeatingChar.java
+++ FirstNonRepeatingChar.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class FirstNonRepeatingChar {
     public static void main(String[] args) {
-        String s = ""
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a string ");
+        String s = in.next();
+        int index = -1;
+        char fnc = ' ';
+
+        if(s.isEmpty()){
+            System.out.println("Empty string");
+        }
+
+        for(char i : s.toCharArray()){
+            if(s.indexOf(i)==s.lastIndexOf(i)){
+                fnc = i;
+                break;
+            }
+            else{
+                index++;
+            }
+        }
+
+        if(index==s.length()){
+            System.out.println("All characters are repeating");
+        }
+        else{
+            System.out.println("First non repeating character is " + fnc);
+        }
     }
 }
